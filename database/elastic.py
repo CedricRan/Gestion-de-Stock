c04548fc2da7fc2c7f1d5ae0fe1c0f79e972dfc1
CedricRan <herinstainasetevecedric@gmail.com>
2026-06-07 13:57:14 +0300

    o_o

--- database/elastic.py
+++ database/elastic.py
@@ -1,10 +1,5 @@
 from  elasticsearch import Elasticsearch
-"""es = Elasticsearch(
+es = Elasticsearch(
         "https://mario-cbfb91.es.us-central1.gcp.elastic.cloud:443",
         api_key="NFVhN2pKNEIxaUJHUmtfekFqU086RlJ5TU9oT3hQNmpNelBSNWpVR1QwZw==" 
         )        
-"""
-es = Elasticsearch(
-        "https://my-elasticsearch-project-c6fad4.es.us-central1.gcp.elastic.cloud:443",
-        api_key="OTVmY2k1NEJvWVlYMHBxODlRMXg6SDI4T3NTRUR4TGF3aW1SYnhxWk9kQQ==" 
-        )
